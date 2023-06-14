80e0022f8437d5f9406d6466cc3e41f55bd08325
hashvizard <hashvizard@gmail.com>
2023-06-14 22:10:03 +0530

    Refund Status

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -839,19 +839,31 @@ export function convertFirestoreTimestampsToReadableStrings(array) {
   });
 }
 
+export function decodeAndFetchFirstValue(jsonString) {
+  // Decode the JSON string
+  const decodedData = JSON.parse(jsonString);
+
+  // Check if the decoded data is an array and has elements
+  if (Array.isArray(decodedData) && decodedData.length > 0) {
+    // Return the first value from the array
+    return decodedData[0];
+  }
+
+  // Return null if the decoded data is not an array or has no elements
+  return null;
+}
+
+
 export function formatDateTime() {
-  var currentDate = new Date();
-
-  var options = {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-    day: "2-digit",
-    month: "long",
-    year: "numeric",
-  };
-
-  return currentDate.toLocaleString("en-US", options);
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 
